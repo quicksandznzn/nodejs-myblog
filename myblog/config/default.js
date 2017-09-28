94e14ad2910441ec759f9cc52ee0764cb7755045
z n <quicksandzn@gmail.com>
2017-09-28 18:00:45 +0800

    init

---
+++ myblog/config/default.js
@@ -0,0 +1,13 @@
+/**
+ * Created by quicksand on 2017/9/28.
+ */
+
+module.exports = {
+  port: 3000,
+  session: {
+    secret: 'myblog',
+    key: 'myblog',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://localhost:27017/myblog'
+};
